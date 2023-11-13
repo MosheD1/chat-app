00f32d9814c0c80ad959b2362045016e4e7c8450
moshed1 <moshedego@gmail.com>
2023-11-13 22:31:49 +0200

    add socket connection

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import { Server } from "socket.io";
 import express from "express";
-
+import { createServer } from 'node:http';
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Chat app</h1>');
+    res.sendFile("C:\\Users\\user\\Documents\\github\\chat-app\\index.html");
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    console.log('socket is on')
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+      });
+});
+
+server.listen(3000, () => {
     console.log('server is running');
 })
